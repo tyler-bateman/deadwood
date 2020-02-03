e38f03dcb65a683950c88d5a2a3c0e557e28c454
tyler <tyler@10.0.0.141>
2020-02-02 21:31:50 -0800

    Added fields and method headers for Board.java

--- Deadwood/src/deadwood/Board.java
+++ Deadwood/src/deadwood/Board.java
@@ -10,5 +10,56 @@ package deadwood;
  * @author tyler
  */
 public class Board {
+    private Space[] spaces;
+    private Player[] players;
+    private int day;
+    private Player activePlayer;
     
+    public void setup() {
+        
+    }
+    
+    public boolean checkForDayEnd() {
+        return false;
+    }
+    
+    public void newDay() {
+        
+    }
+    
+    public Player nextTurn() {
+        return null;
+    }
+    
+    public Space[] getSpaces() {
+        return null;
+    }
+    
+    public void setSpaces(Space[] spaces) {
+        
+    }
+    
+    public Player[] getPlayers() {
+        return null;
+    }
+    
+    public void setPlayers(Player[] players) {
+        
+    }
+    
+    public int getDay() {
+        return 0;
+    }
+    
+    public void setDay() {
+        
+    }
+    
+    public Player getActivePlayer() {
+        return null;
+    }
+    
+    public void setActivePlayer(Player player) {
+        
+    }
 }
