57c3844f9a5b63f5d3bfda9397b7a1e95584ea93
tyler <tyler@10.0.0.141>
2020-02-02 21:55:44 -0800

    Added fields and method headers for Player.java

--- Deadwood/src/deadwood/Player.java
+++ Deadwood/src/deadwood/Player.java
@@ -10,5 +10,73 @@ package deadwood;
  * @author tyler
  */
 public class Player {
+    private int ID;
+    private String name;
+    private int location;
+    private int money;
+    private int credits;
+    private int rank;
+    private Role currRole;
+    private int practiceChips;
     
+    public Player() {
+        
+    }
+    public Player(int ID) {
+        
+    }
+    public Player(int ID, String name){
+        
+    }
+    public void endTurn() {
+        
+    }
+    public boolean isActive() {
+        return false;
+    }
+    public int getScore() {
+        return 0;
+    }
+    public int getID() {
+        return 0;
+    }
+    public String getName() {
+        return null;
+    }
+    public int getLocation() {
+        return 0;
+    }
+    public void setLocation(int newLocID) {
+        
+    }
+    public int getDollars() {
+        return 0;
+    }
+    public void setDollars(int amount) {
+        
+    }
+    public int getCredits() {
+        return 0;
+    }
+    public void setCredits(int amount) {
+        
+    }
+    public int getRank() {
+        return 0;
+    }
+    public void setRank(int rank) {
+        
+    }
+    public Role getRole() {
+        return null;
+    }
+    public void setRole(Role role) {
+        
+    }
+    public int getRehearsalChips() {
+        return 0;
+    }
+    public void setRehearsal(int amount) {
+        
+    }
 }
