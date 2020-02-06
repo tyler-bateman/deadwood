8f3aac2afb13595c1925a4430d95866249a64ca0
NadaBoukhari <51091307+NadaBoukhari@users.noreply.github.com>
2020-02-05 21:02:31 -0800

    Added attributes and empty methods

--- Deadwood/src/deadwood/Scene.java
+++ Deadwood/src/deadwood/Scene.java
@@ -7,8 +7,84 @@ package deadwood;
 
 /**
  *
- * @author tyler
+ * @author nada
  */
 public class Scene {
+    private String name;
+    private SceneCard card;
+    private int remainingShots;
+    private Role[] offCardRoles;
+    
+    public Scene(){
+        
+    }
+    
+    public Scene(String name){
+        this.name = name;
+    }
+    
+    public void resetShots(){
+        
+    }
+    
+    public void close(){
+        
+    }
+    
+    public boolean requestRole(Player player, Role role){
+        return false;
+    }
+    
+    public boolean requestRehearsle(Player player){
+        return false;
+    }
+    
+    public boolean requestActAttempt(Player player){
+        return false;
+    }
+    
+    public void assignDice(){
+        
+    }
+    
+    public boolean isSceneActive(){
+        return false;
+    }
+    
+    protected void wrap(){
+        
+    }
+    
+    public String getName(){
+        return null;
+    }
+    
+    public void setName(){
+        
+    }
+    
+    public SceneCard getCard(){
+        return null;
+    }
+    
+    public void setCard(){
+        
+    }
+    
+    public int getRemainingShots(){
+        return 0;
+    }
+    
+    public void setRemainingShots(){
+        
+    }
+    
+    public Role[] getOffCardRoles(){
+        return null;
+    }
+    
+    public void setOffCardRoles(){
+        
+    }
     
 }
