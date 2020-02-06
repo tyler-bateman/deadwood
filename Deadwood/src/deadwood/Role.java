18ecc48a9ea84538bd07bf759326f8210bc416f0
NadaBoukhari <51091307+NadaBoukhari@users.noreply.github.com>
2020-02-05 21:15:22 -0800

    Added attributes and empty methods

--- Deadwood/src/deadwood/Role.java
+++ Deadwood/src/deadwood/Role.java
@@ -7,8 +7,95 @@ package deadwood;
 
 /**
  *
- * @author tyler
+ * @author nada
  */
 public class Role {
+    private String name;
+    private String description;
+    private int ID;
+    private int rank;
+    private Player occupant;
+    private int payout;
+    
+    public Role(){
+        
+    }
+    
+    public Role(int ID){
+        this.ID = ID;
+    }
+    
+    public Role(int ID, int rank){
+        this.ID = ID;
+        this.rank = rank;
+    }
+    
+    public boolean requestRole(Player player){
+        return false;
+    }
+    
+    public void increasePayout(int amount){
+        
+    }
+    
+    public void removePlayer(){
+        
+    }
+    
+    public int payBonus(){
+        return 0;
+    }
+    
+    public boolean isRoleTaken(){
+        
+    }
+    
+    public String getName(){
+        return null;
+    }
+    
+    public void setName(){
+        
+    }
+    
+    public String getDescription(){
+        return null;
+    }
+    
+    public void setDescription(){
+        
+    }
+    
+    public int getID(){
+        return 0;
+    }
+    
+    public void setID(){
+        
+    }
+    
+    public int getRank(){
+        return 0;
+    }
+    
+    public void setRank(){
+        
+    }
+    
+    public Player getOccupant(){
+        return null;
+    }
+    
+    public void setOccupant(){
+    
+    }
+    
+    public int getPayout(){
+        return 0;
+    }
+    
+    public void setPayout(){
+        
+    }
     
 }
