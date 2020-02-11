1bfeb135dd109bb59ad182728ec4ca62c4842f48
Tyler Bateman <batemat2@wwu.edu>
2020-02-11 11:17:57 -0800

    Implemented isDayOver() method in Board.java

--- Deadwood/src/deadwood/Board.java
+++ Deadwood/src/deadwood/Board.java
@@ -15,56 +15,63 @@ public class Board {
     private static Player[] players;
     private static int day;
     private static Player activePlayer;
-    
+
     public static void setup() {
-        
+
     }
-    
-    public static boolean checkForDayEnd() {
-        return false;
+
+
+    public static boolean isDayOver() {
+        int activeScenes = 0;
+        for(Scene scene: scenes) {
+          if(SceneCard != null) {
+            activeScenes++;
+          }
+        }
+        return activeScenes <= 1;
     }
-    
+
     public static void newDay() {
-        
+
     }
-    
+
     public static Player nextTurn() {
         return null;
     }
-    
+
     public static Space[] getSpaces() {
         return null;
     }
-    
+
     public static void setSpaces(Space[] spaces) {
-        
+
     }
-    
+
     public static Scene[] getScenes() {
         return null;
     }
-    
+
     public static Player[] getPlayers() {
         return null;
     }
-    
+
     public static void setPlayers(Player[] players) {
-        
+
     }
-    
+
     public static int getDay() {
         return 0;
     }
-    
+
     public static void setDay() {
-        
+
     }
-    
+
     public static Player getActivePlayer() {
         return null;
     }
-    
+
     public static void setActivePlayer(Player player) {
-        
+
     }
 }
