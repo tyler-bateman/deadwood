f5a231ecb0c03fe08221f63733baf6391bc13f55
tyler <tyler@10.0.0.141>
2020-02-10 19:31:42 -0800

    Implemented shuffle() method in Deck.java

--- Deadwood/src/deadwood/Deck.java
+++ Deadwood/src/deadwood/Deck.java
@@ -5,19 +5,25 @@
  */
 package deadwood;
 import java.util.Stack;
+import java.util.Random;
 /**
  *
- * @author nada
+ * @author nada & tyler
  */
 public class Deck {
     private Stack<SceneCard> cards;
     
     public Deck(){
-        
+        //TODO: Parse XML
     }
     
     public void shuffle(){
-        
+        Stack<SceneCard> newCards = new Stack<SceneCard>();
+        Random r = new Random();
+        while(!cards.isEmpty()) {
+            newCards.push(cards.remove(r.nextInt(cards.size())));
+        }
+        setCards(newCards);
     }
     
     public void deal(){
@@ -32,7 +38,7 @@ public class Deck {
         return null;
     }
     
-    public void setCards(){
+    public void setCards(Stack<SceneCard> newCards){
         
     }
 }
