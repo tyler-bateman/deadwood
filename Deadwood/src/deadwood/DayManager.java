0a1db3d5753a1b8a6f0d043b4e62cfe8af5f3ab0
Tyler Bateman <batemat2@wwu.edu>
2020-02-11 11:32:28 -0800

    Completed DayManager.java

--- Deadwood/src/deadwood/DayManager.java
+++ Deadwood/src/deadwood/DayManager.java
@@ -8,26 +8,31 @@ public class DayManager{
   private static int day;
 
   /**
+    * Resets the board for the new day if there is only one active scene left.
     * Returns true if there is only one active scene left on the board
     */
-  public static boolean isDayOver() {
+  public static boolean checkForDayEnd() {
       int activeScenes = 0;
       for(Scene scene: Board.getScenes()) {
         if(SceneCard != null) {
           activeScenes++;
         }
       }
-      return activeScenes <= 1;
+      if(activeScenes <= 1) {
+        Board.newDay();
+        return true;
+        setDay(getDay() + 1);
+      }
   }
 
   /**
     * Returns the current day
     */
   public static int getDay() {
-      return 0;
+      return day;
   }
 
   private static void setDay(int day) {
-
+      this.day = day;
   }
 }
