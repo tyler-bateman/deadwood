01adc4687ccb5f79a58ccf55276aeba9276f5a16
tyler <tyler@10.0.0.141>
2020-02-10 19:21:52 -0800

    Implemented hasNextDie() and hasNext() methods in Dice.java and added test cases to DiceTest.java

--- Deadwood/test/DiceTest.java
+++ Deadwood/test/DiceTest.java
@@ -12,10 +12,33 @@ import deadwood.Dice;
 public class DiceTest {
     public static void main(String[] args) {
         test1();
+        test2();
+        test3();
     }
     
     public static void test1() {
-        Dice d = new Dice(6);
+        Dice d = new Dice(10);
+        System.out.println(d.getDice().toString());
+    }
+    
+    public static void test2() {
+        Dice d = new Dice();
+        d.insertDie(6);
+        d.insertDie(5);
+        d.insertDie(4);
+        d.insertDie(3);
+        d.insertDie(2);
+        d.insertDie(1);
+        System.out.println(d.getDice().toString());
+    }
+    
+    public static void test3() {
+        Dice d = new Dice();
+        d.insertDie(2);
+        d.insertDie(4);
+        d.insertDie(3);
+        d.insertDie(1);
+        d.insertDie(5);
         System.out.println(d.getDice().toString());
     }
 }
