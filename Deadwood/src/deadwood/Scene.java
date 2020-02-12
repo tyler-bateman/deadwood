42c815de13fa4c9252aab295d5c3621b38070a11
Tyler Bateman <batemat2@wwu.edu>
2020-02-12 14:38:27 -0800

    Completed several methods

--- Deadwood/src/deadwood/Scene.java
+++ Deadwood/src/deadwood/Scene.java
@@ -13,78 +13,104 @@ public class Scene {
     private String name;
     private SceneCard card;
     private int remainingShots;
+    private int totalShots;
     private Role[] offCardRoles;
-    
+
     public Scene(){
-        
+        //XML
     }
-    
+
     public Scene(String name){
         this.name = name;
     }
-    
+
     public void resetShots(){
-        
+        remainingShots = totalShots;
     }
-    
+
     public void close(){
-        
+        for(Role r: card.getRoles()) {
+          r.removePlayer();
+        }
+        for(Role r: offCardRoles) {
+          r.removePlayer();
+        }
+        card = null;
     }
-    
+
     public boolean requestRole(Player player, Role role){
+        if(this.hasRole(role)) {
+          return role.requestRole(player);
+        } else {
+          return false;
+        }
+    }
+
+    public boolean hasRole(Role role) {
+        boolean result = false;
+        for(Role r: offCardRoles) {
+          if(r == role) {
+            return true;
+          }
+        }
+        for(Roles: card.getRoles()) {
+          if(r == role) {
+            return true;
+          }
+        }
         return false;
     }
-    
-    public boolean requestRehearsle(Player player){
+
+    public boolean requestRehearsal(Player player){
         return false;
     }
-    
+
     public boolean requestActAttempt(Player player){
         return false;
     }
-    
+
     public void assignDice(){
-        
+
     }
-    
+
     public boolean isSceneActive(){
         return false;
     }
-    
+
     protected void wrap(){
-        
+
     }
-    
+
     public String getName(){
         return null;
     }
-    
+
     public void setName(){
-        
+
     }
-    
+
     public SceneCard getCard(){
         return null;
     }
-    
+
     public void setCard(SceneCard card){
-        
+
     }
-    
+
     public int getRemainingShots(){
         return 0;
     }
-    
+
     public void setRemainingShots(){
-        
+
     }
-    
+
     public Role[] getOffCardRoles(){
         return null;
     }
-    
+
     public void setOffCardRoles(){
-        
+
     }
-    
+
 }
