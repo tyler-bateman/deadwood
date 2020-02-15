0686a453d7cd3d8b65552fe1630f30c41817895b
NadaBoukhari <51091307+NadaBoukhari@users.noreply.github.com>
2020-02-15 15:08:24 -0800

    Updated to initialize the board and the deck

--- Deadwood/src/deadwood/Deadwood.java
+++ Deadwood/src/deadwood/Deadwood.java
@@ -5,15 +5,36 @@
  */
 package deadwood;
 
+import java.util.Stack;
+import org.w3c.dom.Document;
+
 /**
  *
- * @author tyler
+ * @author Nada
  */
 public class Deadwood {
     
-    
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         
-    }
-    
+        Stack<SceneCard> deck = new Stack<SceneCard>();
+        Board board = new Board();
+        
+        Document boardDoc = null;
+        Document cardsDoc = null;
+        ParseXML parsing = new ParseXML();
+        try{
+      
+            boardDoc = parsing.getDocFromFile("board.xml");
+            board = parsing.buildBoard(boardDoc);
+        
+            cardsDoc = parsing.getDocFromFile("cards.xml");
+            deck = parsing.buildDeck(cardsDoc);       
+      
+        }catch (Exception e){
+        System.out.println("Error = "+e);   
+        }
+    }   
 }
