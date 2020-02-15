e5c06b137d42e4c2fc3d95824eade6eed61fd291
NadaBoukhari <51091307+NadaBoukhari@users.noreply.github.com>
2020-02-15 15:07:14 -0800

    Update CastingOffice.java

--- Deadwood/src/deadwood/CastingOffice.java
+++ Deadwood/src/deadwood/CastingOffice.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package deadwood;
 import java.lang.ArrayIndexOutOfBoundsException;
 /**
@@ -44,3 +40,4 @@ public class CastingOffice extends Space{
         
     }
 }
+
