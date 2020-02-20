e86ec5b5ef44e3104157be68094d3c60f7fdbeab
tyler <tyler@10.0.0.141>
2020-02-20 14:54:57 -0800

    Added formal method header comments

--- Deadwood/src/deadwood/Role.java
+++ Deadwood/src/deadwood/Role.java
@@ -22,6 +22,7 @@ public class Role {
       * If the role is not taken and the player has the appropriate rank,
       * the player occupies this role.
       * @param player: the player requesting the role
+      * @return true if the player successfully took this role
       * Precondition: The player is in the space that contains this role.
       */
     public boolean requestRole(Player player){
@@ -35,6 +36,7 @@ public class Role {
 
     /**
       * Increases the payout for this role
+      * @param amount: the amount by which to increase the payout
       */
     public void increasePayout(int amount){
         payout += amount;
@@ -53,6 +55,7 @@ public class Role {
 
     /**
       * Pays the bonus for this role
+      * @return the amount that was payed out
       */
     public int payBonus(){
         if(occupant != null) {
@@ -66,55 +69,106 @@ public class Role {
           return 0;
         }
     }
-
+    
+    /**
+     * @return true if the role is taken
+     */
     public boolean isRoleTaken(){
         return occupant != null;
     }
-
+    
+    /**
+     * 
+     * @return the name of this role
+     */
     public String getName(){
         return name;
     }
-
+    
+    /**
+     * 
+     * @param n the new name for this role 
+     */
     public void setName(String n){
         name = n;
     }
-
+    
+    /**
+     *
+     * @return the line for this role
+     */
     public String getLine(){
         return line;
     }
-
+    
+    /**
+     * 
+     * @param l the new line for this role
+     */
     public void setLine(String l){
         line = l;
     }
-
+    
+    /**
+     * 
+     * @return the id of this role
+     */
     public int getID(){
         return ID;
     }
-
+    
+    /**
+     * 
+     * @param newID the new ID for this role
+     */
     private void setID(int newID){
         ID = newID;
     }
 
+    /**
+     * 
+     * @return the rank required to take this role
+     */
     public int getRank(){
         return rank;
     }
-
+    
+    /**
+     * 
+     * @param r the new rank required to take this role
+     */
     public void setRank(int r){
         rank = r;
     }
-
+    
+    /**
+     * 
+     * @return the occupant of this role
+     */
     public Player getOccupant(){
         return occupant;
     }
-
+    
+    /**
+     * 
+     * @param p the new occupant of this role
+     */
     public void setOccupant(Player p){
         occupant = p;
     }
-
+    
+    /**
+     * 
+     * @return the amount that this role will pay as a bonus
+     */
     public int getPayout(){
         return payout;
     }
 
+    /**
+     * 
+     * @param newPayout the new amount that this role pays as a bonus
+     */
     private void setPayout(int newPayout){
         payout = newPayout;
     }
