8f6a43fa3d6fe76a08512738aa8d0659993d879b
tyler <tyler@10.0.0.141>
2020-02-20 23:58:06 -0800

    Added formal method header comments and made minor modifications to a couple of methods

--- Deadwood/src/deadwood/ScoreManager.java
+++ Deadwood/src/deadwood/ScoreManager.java
@@ -14,6 +14,10 @@ public class ScoreManager {
     private static int[] playerScores = new int[8];
     
     
+    /**
+     * Decides on a winner at the end of the game
+     * @param playerList the list of players
+     */
     public static void declareWinner(Player[] playerList){  //ends the game
         calculateScores(playerList);
         int maxScore = 0;
@@ -29,25 +33,50 @@ public class ScoreManager {
         //TODO: Notify controller
     }
     
-    public static void calculateScores(Player[] playerList) {
+    /**
+     * Calculates the score of each player in the list
+     * @param playerList An array containing the players
+     * @return the updated array of player scores
+     */
+    public static int[] calculateScores(Player[] playerList) {
         for(Player p: playerList) {
             calculateScore(p);
         }
+        return getPlayerScores();
     }
     
-    public static void calculateScore(Player p) {
-        playerScores[p.getID()] = p.getCredits() + p.getDollars() + p.getRank() * 5;
+    /**
+     * Calculates the score of a given player
+     * @param p the player whose score is to be calculated
+     * returns the new score for the player
+     */
+    public static int calculateScore(Player p) {
+        return playerScores[p.getID()] = p.getCredits() + p.getDollars() + p.getRank() * 5;
     }
     
-               
+    
+    /**
+     * Gets the array containing the player scores
+     * @return the array containing the scores
+     */
     public static int[] getPlayerScores(){
         return playerScores;
     }
     
+    /**
+     * Gets the score of a specific player
+     * @param ID the ID of the player, i.e. their index in the player array
+     * @return the score of given player
+     */
     public static int getScore(int ID) {
         return playerScores[ID];
     }
     
+    /**
+     * Gets the score of a specific player
+     * @param p the player whose score will be retrieved
+     * @return the score of the given player
+     */
     public static int getScore(Player p) {
         return playerScores[p.getID()];
     }
