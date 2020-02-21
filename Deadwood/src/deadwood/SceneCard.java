7fdb655a5bc03421e0784ac68ef1ac5a35902998
tyler <tyler@10.0.0.141>
2020-02-20 19:27:01 -0800

    Added formal method header comments

--- Deadwood/src/deadwood/SceneCard.java
+++ Deadwood/src/deadwood/SceneCard.java
@@ -18,11 +18,17 @@ public class SceneCard {
     private int budget;
     private boolean faceUp;
 
-    
+    /**
+     * Flips the card, i.e. sets faceUp to true
+     */
     public void flip(){
         faceUp = true;
     }
-
+    
+    /**
+     * Determines whether the card has any players on it
+     * @return true if this card has any players in it
+     */
     public boolean hasPlayers(){
         for(Role r: roles) {
             if(r.isRoleTaken()) {
@@ -31,7 +37,12 @@ public class SceneCard {
         }
         return false;
     }
-
+    
+    /**
+     * Checks whether the card has a given role
+     * @param role the role to be searched for
+     * @return true if the card contains the given role
+     */
     public boolean hasRole(Role role) {
       for(Role r: roles) {
         if(r == role) {
@@ -40,54 +51,99 @@ public class SceneCard {
       }
       return false;
     }
-
+    
+    /**
+     * gets the number
+     * @return the number
+     */
     public int getNumber(){
         return number;
     }
-
+    
+    /**
+     * Sets the number
+     * @param newNumber the new number
+     */
     public void setNumber(int newNumber){
         number = newNumber;
     }
-
+    
+    /**
+     * Gets the name of this scene card
+     * @return the name of this scene card
+     */
     public String getName(){
         return name;
     }
-
+    
+    /**
+     * Sets the name of this scene card
+     * @param n the new name
+     */
     public void setName(String n){
         name = n;
     }
-
+    
+    /**
+     * Gets the description of this scene card
+     * @return the description
+     */
     public String getDescription(){
         return description;
     }
-
+    
+    /**
+     * Sets the description of this scene card
+     * @param d the new description
+     */
     public void setDescription(String d){
         description = d;
     }
 
     /**
-      * Returns a list of roles sorted by rank high to low
+      * @return the list of roles sorted by rank from high to low
       */
     public LinkedList<Role> getRoles(){
         return roles;
     }
-
+    
+    /**
+     * Sets the roleList
+     * @param roleList the new roleList
+     * precondition: roleList is sorted by rank from high to low
+     */
     public void setRoles(LinkedList<Role> roleList){
         roles = roleList;
     }
-
+    
+    /**
+     * Gets the budget of the scene card
+     * @return the budget
+     */
     public int getBudget(){
         return budget;
     }
-
+    
+    /**
+     * Sets the budget of the scene card
+     * @param b the new budget
+     */
     public void setBudget(int b){
         budget = b;
     }
-
+    
+    /**
+     * Returns whether this card is face-up
+     * @return whether his card is face-up
+     */
     public boolean getFaceUp(){
         return faceUp;
     }
-
+    
+    /**
+     * SEts whether this card is face-up
+     * @param b the new boolean indicating whether the card is face-up
+     */
     public void setFaceUp(boolean b){
         faceUp = b;
     }
