a89a023c5c1683cf032031380212e51f354f84a2
tyler <tyler@10.0.0.141>
2020-03-03 11:43:03 -0800

    Implemented Singleton design pattern for Controller

--- Deadwood/src/deadwood/Controller.java
+++ Deadwood/src/deadwood/Controller.java
@@ -1,10 +1,31 @@
 package deadwood;
 
 /**
- *
+ * 
+ * 
+ * Implements Singleton design pattern
  * @author tyler
  */
-public class Controller {
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class Controller extends Observable implements Observer {
+    private static Controller instance = new Controller();
     
+    private Controller() {
+        
+    }
+    
+    public static Controller getInstance() {
+        return instance;
+    }
+    
+    
+    
+    @Override
+    public void update(Observable o, Object obj) {
+        //TODO
+    }
     
 }
