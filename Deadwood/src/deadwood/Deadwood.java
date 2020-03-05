182f9434ecd3e92544dc87a0690fcb69775f037a
tyler <tyler@10.0.0.141>
2020-03-04 22:13:09 -0800

    Cleaned up Deadwood.java nice and purdy like

--- Deadwood/src/deadwood/Deadwood.java
+++ Deadwood/src/deadwood/Deadwood.java
@@ -10,9 +10,13 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.Stack;
+import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 
 /**
+ *
+ * @author Nada
+ *//**
  *
  * @author Nada
  */
@@ -21,63 +25,47 @@ public class Deadwood {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        int numberOfPlayers;
-//        int menuChoice;
-//        int moveChoice;
-//        int afterMoveChoice;
-//        int roleChoice;
-        Player players[];
-
-        Deck deck = Deck.getInstance();
-        Scanner sc = new Scanner(System.in);
-        Stack<SceneCard> stackOfCards = new Stack<SceneCard>();
-        Board board = Board.getInstance();
-        CastingOffice castingOffice = CastingOffice.getInstance();
+    public static void main(String[] args) throws ParserConfigurationException {
        
-
+        ///////XML PARSING PREP///////
         Document boardDoc = null;
         Document cardsDoc = null;
         ParseXML parsing = new ParseXML();
-        try {
-
-            boardDoc = parsing.getDocFromFile("board.xml");
-            board = parsing.buildBoard(boardDoc);
-            parsing.buildAdjacentSpaces(boardDoc, board);
-
-            cardsDoc = parsing.getDocFromFile("cards.xml");
-            stackOfCards = parsing.buildDeck(cardsDoc);
-            deck.initDeck(stackOfCards); // it is initialized and shuffled
-
-        } catch (Exception e) {
-            System.out.println("Error = " + e);
+        //////////////////////////////
+        
+        ///////SET UP BOARD AND SPACE ADJACENCY///////
+        boardDoc = parsing.getDocFromFile("board.xml");
+        Board board = parsing.buildBoard(boardDoc);
+        parsing.buildAdjacentSpaces(boardDoc, board);
+        //////////////////////////////////////////////
+        
+        ///////INITIALIZE CARDS AND DECK////////
+        cardsDoc = parsing.getDocFromFile("cards.xml");
+        Stack<SceneCard> stackOfCards = parsing.buildDeck(cardsDoc);
+        Deck deck = Deck.getInstance();
+        deck.initDeck(stackOfCards); 
+        ////////////////////////////////////////
+        
+        ///////INITIALIZE PLAYERS///////
+        int numberOfPlayers;                            //TODO!!!
+        if(args.length > 0 ) {                          //We need some sort of window to select the number of players
+            numberOfPlayers = Integer.parseInt(args[0]);//We'll figure it out
+        } else {                                        
+            numberOfPlayers = 2;                        
         }
-
-        System.out.println("Welcome to Deadwood ! \n");
-//        if (args.length != 0) {
-            numberOfPlayers = Integer.parseInt(args[0]);
-//        } else {
-//            do {
-//                System.out.println("Please input the number of players: ");
-//                numberOfPlayers = sc.nextInt();
-//
-//            } while (!isPlayerNumberValid(numberOfPlayers));
-//        }
-
-        players = new Player[numberOfPlayers];
-        ScoreManager.init(numberOfPlayers);
-
+        Player[] players = new Player[numberOfPlayers];
+        
         for (int i = 0; i < numberOfPlayers; i++) {
             Player player = new Player(i, board.getTrailorsID());
             players[i] = player;
-
         }
-
-        board.setPlayers(players);
-
-        DayManager.getInstance().init(numberOfPlayers);
-
+        ////////////////////////////////
         
+        ///////CONNECT PLAYERS TO SYSTEM///////
+        board.setPlayers(players);
+        DayManager.getInstance().init(numberOfPlayers);
+        ScoreManager.init(numberOfPlayers);
+        ///////////////////////////////////////
     }
 }
 
