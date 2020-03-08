9c3e15c6b61b348f7934f636c87a82f0dcd7ac0b
Curveball <Curveball@10.111.147.72>
2020-03-07 17:33:52 -0800

    Made changes to demo method

--- Deadwood/src/deadwood/BoardPane.java
+++ Deadwood/src/deadwood/BoardPane.java
@@ -13,18 +13,99 @@ import javax.swing.*;
  * @author Curveball
  */
 public class BoardPane extends JLayeredPane {
+
     private ImageIcon boardIcon;
     private JLabel boardLabel;
-    
+    private JLabel[] playerLabels;
+
     BoardPane(int height, int width, ImageIcon boardIcon) {
-        
+
         this.boardIcon = boardIcon;
+        playerLabels = new JLabel[2];
         setLayout(null);
         setPreferredSize(new Dimension(900, 1200));
         boardLabel = new JLabel();
         boardLabel.setIcon(boardIcon);
         boardLabel.setBounds(0, 0, boardIcon.getIconWidth(), boardIcon.getIconHeight());
         add(boardLabel, new Integer(0));
+        
+        Demo();
     }
-    
+
+    private void Demo() {
+        Board board = Board.getInstance();
+        Deck deck = Deck.getInstance();
+
+        ////DEALING CARDBACKS DEMO
+        for (int i = 0; i < board.getScenes().length; i++) {
+            JLabel backlabel = new JLabel();
+            ImageIcon backIcon = new ImageIcon(getClass().getResource("/resources/CardBack.jpg"));
+            backlabel.setIcon(backIcon);
+            backlabel.setBounds(board.getScene(i).getXCoordinates(), board.getScene(i).getYCoordinates(), backIcon.getIconWidth(), backIcon.getIconHeight());
+            backlabel.setOpaque(false);
+
+            add(backlabel, new Integer(2));
+
+            /// DEALING CARD FACES DEMO
+            JLabel cardlabel = new JLabel();
+            SceneCard card = deck.getCards().pop();
+            String cardImage = card.getIconID();
+            ImageIcon cIcon = new ImageIcon(getClass().getResource("/resources/" + cardImage));
+            cardlabel.setIcon(cIcon);
+            cardlabel.setBounds(board.getScene(i).getXCoordinates(), board.getScene(i).getYCoordinates(), cIcon.getIconWidth(), cIcon.getIconHeight());
+            cardlabel.setOpaque(false);
+
+            add(cardlabel, new Integer(1));
+
+            /// SETTING SHOT COUNTERS
+            for (int j = 0; j < board.getScene(i).getShotCountersXCoordinates().size(); j++) {
+                JLabel shotCounterLabel = new JLabel();
+                ImageIcon shotCounterIcon = new ImageIcon(getClass().getResource("/resources/shot.png"));
+                shotCounterLabel.setIcon(shotCounterIcon);
+                shotCounterLabel.setBounds(board.getScene(i).getShotCountersXCoordinates().get(j), board.getScene(i).getShotCountersYCoordinates().get(j), shotCounterIcon.getIconWidth(), shotCounterIcon.getIconHeight());
+                shotCounterLabel.setOpaque(false);
+                add(shotCounterLabel, new Integer(2));
+            }
+
+            /// MOVING PLAYER ICON TO ON CARD ROLES
+            /*for (int m = 0; m < card.getRoles().size(); m++) {
+                JLabel playerLabel1 = new JLabel();
+                ImageIcon playerIcon1 = new ImageIcon(getClass().getResource("/resources/r1.png"));
+                playerLabel1.setIcon(playerIcon1);
+                playerLabel1.setBounds(card.getRoles().get(m).getXCoordinates(), card.getRoles().get(m).getYCoordinates(), playerIcon1.getIconWidth(), playerIcon1.getIconHeight());
+                playerLabel1.setOpaque(true);
+                cardlabel.add(playerLabel1);
+
+            }*/
+            /// MOVING PLAYER TO OFF CARD ROLE
+            /*for (int j = 0; j < board.getScene(i).getOffCardRoles().size(); j++) {
+                JLabel playerLabel = new JLabel();
+                ImageIcon playerIcon = new ImageIcon(getClass().getResource("/resources/b1.png"));
+                playerLabel.setIcon(playerIcon);
+                playerLabel.setBounds(board.getScene(i).getOffCardRoles().get(j).getXCoordinates(), board.getScene(i).getOffCardRoles().get(j).getYCoordinates(), playerIcon.getIconWidth(), playerIcon.getIconHeight());
+                playerLabel.setOpaque(false);
+                boardPane.add(playerLabel, new Integer(2));
+            }*/
+        }
+
+        for (int i = 0; i < 2; i++) {
+
+            JLabel playerLabel = new JLabel();      
+            if (i == 0) {
+                ImageIcon playerIcon = new ImageIcon(getClass().getResource("/resources/r1.png"));
+                playerLabel.setIcon(playerIcon);
+                playerLabel.setBounds(board.getSpace(10).getXCoordinates(), board.getSpace(10).getYCoordinates(), playerIcon.getIconWidth(), playerIcon.getIconHeight());
+
+            } else {
+                ImageIcon playerIcon = new ImageIcon(getClass().getResource("/resources/b1.png"));
+                playerLabel.setIcon(playerIcon);
+                playerLabel.setBounds(board.getSpace(10).getXCoordinates() + 50, board.getSpace(10).getYCoordinates(), playerIcon.getIconWidth(), playerIcon.getIconHeight());
+            }
+            playerLabel.setOpaque(true);
+            playerLabels[i] = playerLabel;
+            
+            add(playerLabels[i], new Integer(2));
+        }
+    }
+
 }
--- Deadwood/src/deadwood/GameView.java
+++ Deadwood/src/deadwood/GameView.java
@@ -27,7 +27,6 @@ public class GameView extends JFrame /*implements ActionListener*/ {
     private ActionsPanel actionsPanel;
     private BoardPane boardPane;
     private InfoPanel infoPanel;   
-    private JLabel[] playerLabels;
     private int playerNumber;
 
     public GameView(int numOfPlayers) {
@@ -39,7 +38,7 @@ public class GameView extends JFrame /*implements ActionListener*/ {
         }
         playerNumber = numOfPlayers;
         
-        playerLabels = new JLabel[numOfPlayers];
+        
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int height = screenSize.height;
         int width = screenSize.width;
@@ -57,7 +56,6 @@ public class GameView extends JFrame /*implements ActionListener*/ {
         jpanel1.add(boardPane, BorderLayout.CENTER);
         jpanel1.add(infoPanel, BorderLayout.EAST);
 
-        Demo();
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(jpanel1);
@@ -66,81 +64,9 @@ public class GameView extends JFrame /*implements ActionListener*/ {
         Image image = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/resources/DeadwoodIcon.PNG"));
         setIconImage(image);
         this.setVisible(true);
-        //this.pack();
 
     }
 
-    private void Demo() {
-        Board board = Board.getInstance();
-        Deck deck = Deck.getInstance();
 
-        ////DEALING CARDBACKS DEMO
-        for (int i = 0; i < board.getScenes().length; i++) {
-            JLabel backlabel = new JLabel();
-            ImageIcon backIcon = new ImageIcon(getClass().getResource("/resources/CardBack.jpg"));
-            backlabel.setIcon(backIcon);
-            backlabel.setBounds(board.getScene(i).getXCoordinates(), board.getScene(i).getYCoordinates(), backIcon.getIconWidth(), backIcon.getIconHeight());
-            backlabel.setOpaque(false);
-
-            boardPane.add(backlabel, new Integer(2));
-
-            /// DEALING CARD FACES DEMO
-            JLabel cardlabel = new JLabel();
-            SceneCard card = deck.getCards().pop();
-            String cardImage = card.getIconID();
-            ImageIcon cIcon = new ImageIcon(getClass().getResource("/resources/" + cardImage));
-            cardlabel.setIcon(cIcon);
-            cardlabel.setBounds(board.getScene(i).getXCoordinates(), board.getScene(i).getYCoordinates(), cIcon.getIconWidth(), cIcon.getIconHeight());
-            cardlabel.setOpaque(false);
-
-            boardPane.add(cardlabel, new Integer(1));
-
-            /// SETTING SHOT COUNTERS
-            for (int j = 0; j < board.getScene(i).getShotCountersXCoordinates().size(); j++) {
-                JLabel shotCounterLabel = new JLabel();
-                ImageIcon shotCounterIcon = new ImageIcon(getClass().getResource("/resources/shot.png"));
-                shotCounterLabel.setIcon(shotCounterIcon);
-                shotCounterLabel.setBounds(board.getScene(i).getShotCountersXCoordinates().get(j), board.getScene(i).getShotCountersYCoordinates().get(j), shotCounterIcon.getIconWidth(), shotCounterIcon.getIconHeight());
-                shotCounterLabel.setOpaque(false);
-                boardPane.add(shotCounterLabel, new Integer(2));
-            }
-
-            /// MOVING PLAYER ICON TO ON CARD ROLES
-            /*for (int m = 0; m < card.getRoles().size(); m++) {
-                JLabel playerLabel1 = new JLabel();
-                ImageIcon playerIcon1 = new ImageIcon(getClass().getResource("/resources/r1.png"));
-                playerLabel1.setIcon(playerIcon1);
-                playerLabel1.setBounds(card.getRoles().get(m).getXCoordinates(), card.getRoles().get(m).getYCoordinates(), playerIcon1.getIconWidth(), playerIcon1.getIconHeight());
-                playerLabel1.setOpaque(true);
-                cardlabel.add(playerLabel1);
-
-            }*/
-            /// MOVING PLAYER TO OFF CARD ROLE
-            /*for (int j = 0; j < board.getScene(i).getOffCardRoles().size(); j++) {
-                JLabel playerLabel = new JLabel();
-                ImageIcon playerIcon = new ImageIcon(getClass().getResource("/resources/b1.png"));
-                playerLabel.setIcon(playerIcon);
-                playerLabel.setBounds(board.getScene(i).getOffCardRoles().get(j).getXCoordinates(), board.getScene(i).getOffCardRoles().get(j).getYCoordinates(), playerIcon.getIconWidth(), playerIcon.getIconHeight());
-                playerLabel.setOpaque(false);
-                boardPane.add(playerLabel, new Integer(2));
-            }*/
-        }
-
-        for (int i = 0; i < playerNumber; i++) {
-
-            JLabel playerLabel = new JLabel();
-            ImageIcon playerIcon = new ImageIcon(getClass().getResource("/resources/r1.png"));
-            playerLabel.setIcon(playerIcon);
-            if (i == 0) {
-                playerLabel.setBounds(board.getSpace(10).getXCoordinates(), board.getSpace(10).getYCoordinates(), playerIcon.getIconWidth(), playerIcon.getIconHeight());
-
-            } else {
-                playerLabel.setBounds(board.getSpace(10).getXCoordinates() + 50, board.getSpace(10).getYCoordinates(), playerIcon.getIconWidth(), playerIcon.getIconHeight());
-            }
-            playerLabels[i] = playerLabel;
-            playerLabel.setOpaque(true);
-            boardPane.add(playerLabel, new Integer(2));
-        }
-    }
 
 }
