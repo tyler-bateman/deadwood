c27a575516e850c464ab24a5bde90c1d8bf0cf4f
Curveball <Curveball@10.111.147.72>
2020-03-08 13:28:46 -0700

    Move buttons show adjacent spaces of the active player

--- Deadwood/src/deadwood/MoveChoicesFrame.java
+++ Deadwood/src/deadwood/MoveChoicesFrame.java
@@ -9,13 +9,15 @@ import static deadwood.ActionsPanel.centreWindow;
 import java.awt.Component;
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 /**
  *
  * @author Curveball
  */
-public class MoveChoicesFrame extends JFrame {
+public class MoveChoicesFrame extends JFrame implements ActionListener {
 
     private JPanel contentPane;
     private JLabel whereToLabel;
@@ -24,7 +26,7 @@ public class MoveChoicesFrame extends JFrame {
 
     public MoveChoicesFrame() {
 
-        sceneButtons = new JButton[3];
+        sceneButtons = new JButton[Board.getInstance().getSpace(TurnManager.getInstance().getActivePlayer().getLocation()).getAdjacentSpaces().size()];
         centreWindow(this);
 
         contentPane = new JPanel();
@@ -37,33 +39,41 @@ public class MoveChoicesFrame extends JFrame {
         scenesPanel = new JPanel();
         scenesPanel.setLayout(new BoxLayout(scenesPanel, BoxLayout.X_AXIS));
 
-        sceneButtons[0] = new JButton("Scene 1");
-        sceneButtons[0].setAlignmentX(Component.CENTER_ALIGNMENT);
-        sceneButtons[1] = new JButton("Scene 2");
-        sceneButtons[1].setAlignmentX(Component.CENTER_ALIGNMENT);
-        sceneButtons[2] = new JButton("Scene 3");
-        sceneButtons[2].setAlignmentX(Component.CENTER_ALIGNMENT);
+        //sceneButtons[0] = new JButton("Scene 1");
+        //sceneButtons[0].setAlignmentX(Component.CENTER_ALIGNMENT);
+        //sceneButtons[0].addActionListener(this);
+        //sceneButtons[1] = new JButton("Scene 2");
+        //sceneButtons[1].setAlignmentX(Component.CENTER_ALIGNMENT);
+        //sceneButtons[2] = new JButton("Scene 3");
+        //sceneButtons[2].setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        scenesPanel.add(Box.createHorizontalGlue());
-        scenesPanel.add(sceneButtons[0]);
-        scenesPanel.add(Box.createHorizontalGlue());
+        //scenesPanel.add(Box.createHorizontalGlue());
+        //scenesPanel.add(sceneButtons[0]);
+        /*scenesPanel.add(Box.createHorizontalGlue());
         scenesPanel.add(sceneButtons[1]);
         scenesPanel.add(Box.createHorizontalGlue());
         scenesPanel.add(sceneButtons[2]);
+        scenesPanel.add(Box.createHorizontalGlue());*/
+        setJButtons();
         scenesPanel.add(Box.createHorizontalGlue());
-
+        
         contentPane.add(Box.createVerticalGlue());
         contentPane.add(whereToLabel);
         contentPane.add(Box.createVerticalGlue());
         contentPane.add(scenesPanel);
         contentPane.add(Box.createVerticalGlue());
-        
-        setFonts();
+
         setContentPane(contentPane);
         setVisible(true);
     }
 
-    private void setFonts() {
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == sceneButtons[0]) {
+
+        }
+    }
+
+    private void setFonts(JButton b) {
         try {
             Font regularFont = Font.createFont(Font.TRUETYPE_FONT, getClass().getResource("/resources/Spartan-Regular.ttf").openStream());
             Font boldFont = Font.createFont(Font.TRUETYPE_FONT, getClass().getResource("/resources/Spartan-Medium.ttf").openStream());
@@ -72,12 +82,22 @@ public class MoveChoicesFrame extends JFrame {
             genv.registerFont(boldFont);
             Font font1 = boldFont.deriveFont(24f);
             Font font2 = regularFont.deriveFont(30f);
-            sceneButtons[0].setFont(font1);
-            sceneButtons[1].setFont(font1);
-            sceneButtons[2].setFont(font1);
+            b.setFont(font1);
             whereToLabel.setFont(font2.deriveFont(Font.ITALIC));
         } catch (Exception ex) {
 
+        }
+    }
+
+    private void setJButtons() {
+        for (int i = 0; i < Board.getInstance().getSpace(TurnManager.getInstance().getActivePlayer().getLocation()).getAdjacentSpaces().size(); i++) {
+            sceneButtons[i] = new JButton(Board.getInstance().getSpace(TurnManager.getInstance().getActivePlayer().getLocation()).getAdjacentSpaces().get(i).getName());
+            sceneButtons[i].setAlignmentX(Component.CENTER_ALIGNMENT);
+            sceneButtons[i].addActionListener(this);
+            scenesPanel.add(Box.createHorizontalGlue());
+            scenesPanel.add(sceneButtons[i]);
+            setFonts(sceneButtons[i]);
+            
         }
     }
 
