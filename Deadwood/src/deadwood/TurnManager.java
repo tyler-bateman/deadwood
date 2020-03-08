1d505662302e2bf4ff0e0fddeccca9c3b016a928
tyler <tyler@10.0.0.141>
2020-03-08 01:02:13 -0800

    TurnManager no longer implements observer

--- Deadwood/src/deadwood/TurnManager.java
+++ Deadwood/src/deadwood/TurnManager.java
@@ -8,7 +8,7 @@ package deadwood;
 
 import java.util.Observable;
 import java.util.Observer;
-public class TurnManager extends Observable implements Observer{
+public class TurnManager extends Observable{
     private static TurnManager instance;
     private int numPlayers;
     private int activePlayerID;
@@ -53,27 +53,5 @@ public class TurnManager extends Observable implements Observer{
         return Board.getInstance().getPlayer(getActivePlayerID());
     }
     
-    /**
-     * 
-     * @param o the Observable that updated this observer
-     * @param obj the new active player ID. If it is invalid, i.e. too large,
-     *            it resets at the first player and notifies the controller.
-     */
-    @Override
-    public void update(Observable o, Object obj) {
-        int newTurn;
-        try{
-            newTurn = (Integer)obj;
-        } catch(ClassCastException e) {
-            return;
-        }
-        
-        if(newTurn >= numPlayers) {
-            activePlayerID = 0;
-            notifyObservers();
-        } else {
-            activePlayerID = newTurn;
-        }
-        
-    }
+    
 }
