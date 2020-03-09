de84abb2a7c3ff05833dc7f82d2cc551a4fc456e
tyler <tyler@10.0.0.141>
2020-03-09 16:28:21 -0700

    Added fields and setters to keep track of what the player has already done on their turn

--- Deadwood/src/deadwood/TurnManager.java
+++ Deadwood/src/deadwood/TurnManager.java
@@ -13,6 +13,13 @@ public class TurnManager extends Observable{
     private int numPlayers;
     private int activePlayerID;
     
+    //The following variables keep track of which actions have been taken by the player this turn
+    private boolean hasTakenRole = false;
+    private boolean hasMoved = false;
+    private boolean hasActed = false;
+    private boolean hasRehearsed = false;
+    private boolean hasUpgraded = false;
+    
     
     /**
      * Private constructor as required by the singleton design pattern.
@@ -63,4 +70,76 @@ public class TurnManager extends Observable{
         }
         notifyObservers();
     }
+    
+    /**
+     * @return true if the active player has taken a role this turn
+     */
+    public boolean hasTakenRole() {
+        return hasTakenRole;
+    }
+    
+    /**
+     * @param b the new flag for whether the active player has taken a role this turn
+     */
+    public void setTakenRole(boolean b) {
+        hasTakenRole = b;
+    }
+    
+    /**
+     * @return true if the active player has moved this turn.
+     */
+    public boolean hasMoved() {
+        return hasMoved;
+    }
+    
+    /**
+     * @param b the new flag for whether the active player has moved this turn
+     */
+    public void setHasMoved(Boolean b) {
+        hasMoved = b;
+    }
+    
+    /**
+     * @return true if the active player has acted this turn
+     */
+    public boolean hasActed() {
+        return hasActed;
+    }
+    
+    /**
+     * @param b the new flag for whether the active player has acted this turn
+     */
+    public void setHasActed(Boolean b) {
+        hasActed = b;
+    }
+    
+    /**
+     * @return true if the active player has acted this turn
+     */
+    public boolean hasUpgraded() {
+        return hasUpgraded;
+    }
+    
+    /**
+     * @param b the new flag for whether the active player has upgraded this turn
+     */
+    public void setHasUpgraded(Boolean b) {
+        hasUpgraded = b;
+    }
+    
+    /**
+     * @return true if the active player has rehearsed this turn
+     */
+    public boolean hasRehearsed() {
+        return hasRehearsed;
+    }
+    
+    /**
+     * @param b the new flag for whether the active player has rehearsed this turn
+     */
+    public void setHasRehearsed(Boolean b) {
+        hasRehearsed = b;
+    }
+    
+    
 }
