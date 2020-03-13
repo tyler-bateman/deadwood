77d7e1aa3c2707d0482d697a0421802607e54727
tyler <tyler@10.0.0.141>
2020-03-13 13:09:18 -0700

    CastingOffice looks nice

--- Deadwood/src/deadwood/CastingOffice.java
+++ Deadwood/src/deadwood/CastingOffice.java
@@ -14,18 +14,37 @@ import java.lang.ArrayIndexOutOfBoundsException;
 import java.util.LinkedList;
 
 public class CastingOffice extends Space{
+    
+    /**
+     * The prices of each rank in dollars and credits starting at rank 2
+     */
     private int[] dollarPrices;
     private int[] creditPrices;
+    
+    /**
+     * The coordinates of the upgrade buttons in the GUI
+     */
     private LinkedList<Integer> dollarCoordinates;
     private LinkedList<Integer> creditCoordinates;
+    
+    /**
+     * The singular instance of CastingOffice
+     */
     private static CastingOffice instance = new CastingOffice();
     
+    /**
+     * The private constructor as required by Singleton design pattern
+     */
     private CastingOffice(){
         dollarPrices = new int[]{4, 10, 18, 28, 40};
         creditPrices = new int[]{5, 10, 15, 20, 25};
         
     }
     
+    /**
+     * 
+     * @return the singular instance of the CastingOffice
+     */
     public static CastingOffice getInstance() {
         return instance;
     }
@@ -38,12 +57,11 @@ public class CastingOffice extends Space{
      */
     public boolean purchaseRank(Player player, int rank, CurrencyType payment) {
         if(rank <= player.getRank() || rank > 6) {
-            //Deadwood.sendMessage("The selected rank is out of range");
             return false;
         }
            
         if(this.containsPlayer(player)) {
-            int price = (payment == CurrencyType.DOLLARS)? getDollarPrice(rank) : getCreditPrice(rank);
+            int price = getPrice(rank, payment);
             boolean successful = Banker.charge(player, price, payment);
             if(successful) {
                 player.setRank(rank);
@@ -58,6 +76,12 @@ public class CastingOffice extends Space{
         return false;
     }
     
+    /**
+     * Returns the maximum rank that can be afforded
+     * @param wealth the amount of dollars or credits that are available to spend
+     * @param payment the type of currency that would be used
+     * @return the maximum rank that can be afforded minus 2
+     */
     public int getMaxRankIndex(int wealth, CurrencyType payment) {
         for(int i = 0; i < dollarPrices.length; i++) {
             if(getPrice(i + 2, payment) > wealth) {
@@ -67,6 +91,12 @@ public class CastingOffice extends Space{
         return 6;
     }
     
+    /**
+     * Gets the price of a desired rank of a certain payment type
+     * @param rank the desired rank
+     * @param payment the payment type for which the price will be retrieved
+     * @return the price of a desired rank of in a certain payment type
+     */
     public int getPrice(int rank, CurrencyType payment) {
         if(payment == CurrencyType.CREDITS) {
             return getCreditPrice(rank);
@@ -117,18 +147,33 @@ public class CastingOffice extends Space{
         return creditPrices;
     }
     
+    /**
+     * @return the coordinates of the dollar buttons in the upgrade menu
+     */
     public LinkedList<Integer> getDollarCoordinates(){
         return dollarCoordinates;
     }
     
+    /**
+     * Sets the coordinates of the dollar buttons in the upgrade menu
+     * @param d A linked list containing the new coordinates
+     */
     public void setDollarCoordinates(LinkedList<Integer> d){
         dollarCoordinates = d;
     }
     
+    /**
+     * Gets the coordinates of the credit buttons in the upgrade menu
+     * @return A linked list containing the coordinates
+     */
     public LinkedList<Integer> getCreditCoordinates(){
         return creditCoordinates;
     }
     
+    /**
+     * Sets the coordinates of the credit buttons in the upgrade menu
+     * @param c A linked list containing the new coordinates
+     */
     public void setCreditCoordinates(LinkedList<Integer> c){
         creditCoordinates = c;
     }
