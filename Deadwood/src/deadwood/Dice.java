ccb56628d383efd643100577f7d992f81c3cc666
tyler <tyler@10.0.0.141>
2020-03-13 13:33:55 -0700

    Dice looks nice

--- Deadwood/src/deadwood/Dice.java
+++ Deadwood/src/deadwood/Dice.java
@@ -1,31 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package deadwood;
 import java.util.LinkedList;
 import java.util.Random;
 
 /**
- *
- * @author nada
+ * Dice.java
+ * This class represents Dice and performs all operations involving Dice in the model
+ * 
+ * @author nada & tyler
  */
 public class Dice {
+    
+    /**
+     * The linked list of dice represented by this dice object
+     */
     private LinkedList<Integer> dice;
     
+    /**
+     * Constructor
+     * Initializes the linked list of dice
+     */
     public Dice(){
-        dice = new LinkedList<Integer>();
+        setDice(new LinkedList<Integer>());
     }
     
+    /**
+     * Constructor
+     * Initializes the linked list of dice and rolls dice (populates the linked list)
+     * @param num the number of dice to roll
+     */
     public Dice(int num){
         dice = new LinkedList<Integer>();
         roll(num);
     }
     
     
-    /*
+    /**
     * Populates dice with num random integers in descending order using insertion sort
+    * @param num: The number of dice to roll
     */
     public void roll(int num){
       
@@ -33,39 +44,59 @@ public class Dice {
        for(int i = 0; i < num; i++) {
            insertDie(r.nextInt(6) + 1);
        }
-       //Deadwood.sendMessage("You rolled the following dice: " + dice.toString());
     }
     
-    /*
-    * Inserts the int val into dice
+    /**
+    * Inserts a die into the dice linked list
+    * Uses the same process as insertion sort to maintain the linked list being in descending order
     * Invariant: dice is in descending order
+    * @param val: the value of the die to be inserted
     */
-    public void insertDie(int val) {
-        dice.addFirst(val);
-        for(int i = 0; i < dice.size() - 1; i++) {
-            if(val < dice.get(i + 1)) {
-                dice.set(i, dice.get(i + 1));
-                dice.set(i + 1, val);
+    private void insertDie(int val) {
+        LinkedList<Integer> d = getDice();
+        d.addFirst(val);
+        for(int i = 0; i < d.size() - 1; i++) {
+            if(val < d.get(i + 1)) {
+                d.set(i, d.get(i + 1));
+                d.set(i + 1, val);
             } else {
                 break;
             }
         }
     }
     
+    /**
+     * Determines if there are any more dice 
+     * @return true if the dice linked list is not empty
+     */
     public boolean hasNextDie() {
         return !dice.isEmpty();
     }
     
+    /**
+     * Removes the first die and returns its value
+     * @return the value of the remaining die with the greatest value
+     */
     public int nextDie() {
         return dice.pop();
     }
-        
-    public LinkedList<Integer> getDice(){
+    
+    /**
+     * Gets the linked list of dice
+     * @return the linked list of dice
+     */
+    private LinkedList<Integer> getDice(){
         return dice;
     }
     
-    public void setDice(){
-        
+    /**
+     * Replaces the dice linked list with a new list of dice
+     * 
+     * Precondition: d is in descending order (or empty)
+     * @param d the new list of dice
+     */
+    private void setDice(LinkedList<Integer> d) {
+        dice = d;
     }
     
 }
